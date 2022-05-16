29aca1d0888445b64a64cf370373258083133827
John Arroyo <johnarroyo45@gmail.com>
2022-05-15 21:07:13 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+const {
+  models: { User },
+} = require("./db");
+const path = require("path");
+
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+
+app.post("/api/auth", async (req, res, next) => {
+  try {
+    res.send({ token: await User.authenticate(req.body) });
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.get("/api/auth", async (req, res, next) => {
+  try {
+    res.send(await User.byToken(req.headers.authorization));
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.delete("/api/auth", async (req, res, next) => {
+  try {
+    res.send();
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(err.status || 500).send({ error: err.message });
+});
+
+module.exports = app;
---
+++ db.js
@@ -0,0 +1,75 @@
+const Sequelize = require("sequelize");
+const { STRING } = Sequelize;
+const config = {
+  logging: false,
+};
+
+if (process.env.LOGGING) {
+  delete config.logging;
+}
+const conn = new Sequelize(
+  process.env.DATABASE_URL || "postgres://localhost/acme_db",
+  config
+);
+
+const User = conn.define("user", {
+  username: STRING,
+  password: STRING,
+});
+
+User.byToken = async (token) => {
+  try {
+    const user = await User.findByPk(token);
+    if (user) {
+      return user;
+    }
+    const error = Error("bad credentials");
+    error.status = 401;
+    throw error;
+  } catch (ex) {
+    const error = Error("bad credentials");
+    error.status = 401;
+    throw error;
+  }
+};
+
+User.authenticate = async ({ username, password }) => {
+  const user = await User.findOne({
+    where: {
+      username,
+      password,
+    },
+  });
+  if (user) {
+    return user.id;
+  }
+  const error = Error("bad credentials");
+  error.status = 401;
+  throw error;
+};
+
+const syncAndSeed = async () => {
+  await conn.sync({ force: true });
+  const credentials = [
+    { username: "lucy", password: "lucy_pw" },
+    { username: "moe", password: "moe_pw" },
+    { username: "larry", password: "larry_pw" },
+  ];
+  const [lucy, moe, larry] = await Promise.all(
+    credentials.map((credential) => User.create(credential))
+  );
+  return {
+    users: {
+      lucy,
+      moe,
+      larry,
+    },
+  };
+};
+
+module.exports = {
+  syncAndSeed,
+  models: {
+    User,
+  },
+};
