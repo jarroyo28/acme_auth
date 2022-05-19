2bb7da639b78fe7c19abdc601c40a2ad20a7f397
John Arroyo <johnarroyo45@gmail.com>
2022-05-18 19:22:02 -0500

    Created custom middleware that handles the jwt verify call for us, attached the users data to the req object

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ const {
 } = require("./db");
 const path = require("path");
 
+const requireToken = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+    const user = await User.byToken(token);
+    req.user = user;
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 
 app.post("/api/auth", async (req, res, next) => {
@@ -16,9 +27,9 @@ app.post("/api/auth", async (req, res, next) => {
   }
 });
 
-app.get("/api/auth", async (req, res, next) => {
+app.get("/api/auth", requireToken, async (req, res, next) => {
   try {
-    res.send(await User.byToken(req.headers.authorization));
+    res.send(req.user);
   } catch (ex) {
     next(ex);
   }
@@ -33,15 +44,12 @@ app.delete("/api/auth", async (req, res, next) => {
 });
 
 // This route is to get a specific user's notes
-app.get("/api/users/:id/notes", async (req, res, next) => {
+app.get("/api/users/:id/notes", requireToken, async (req, res, next) => {
   try {
     // I first find the user via their token
-    const user = await User.byToken(req.headers.authorization);
+    const user = req.user;
     // Then I grab the users notes using a magic method
     const notes = await user.getNotes();
-
-    console.log(user.id);
-    console.log(req.params.id);
 
     if (user.id == req.params.id) {
       res.send(notes);
